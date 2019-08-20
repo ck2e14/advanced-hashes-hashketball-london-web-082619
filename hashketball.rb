cb95655fb25a8dc4d0f25edb416e02897c23b528
Christopher Kennedy <chris94kennedy@gmail.com>
2019-08-20 16:03:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -219,8 +219,8 @@ def big_shoe_rebounds
   biggest_shoe = 0
   number_of_rebounds = 0
 
-  game_hash.each do |_team, game_data|
-    game_data[:players].each do |player|
+  game_hash.each do |home_or_away, team_data|
+    team_data[:players].each do |player|
       if player[:shoe] > biggest_shoe
         biggest_shoe = player[:shoe]
         num_rebounds = player[:rebounds]
