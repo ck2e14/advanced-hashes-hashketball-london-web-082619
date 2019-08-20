b0ce04479617383cc8a21049e7c1ade1a660261b
Christopher Kennedy <chris94kennedy@gmail.com>
2019-08-20 16:29:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -242,8 +242,6 @@ def most_points_scored
         attribute_data.each do |individual_player|
           if individual_player[:points] > starting_points
             starting_points = individual_player[:points]
-          end
-          if individual_player[:points] > starting_points
             top_scorer << individual_player[:player_name]
           end
         end
